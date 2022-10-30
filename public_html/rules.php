473d9c9746a6c151fad7a36383da73a598301d27
botiez <--global>
2022-10-30 21:36:07 +0200

    on init by j j

---
+++ public_html/rules.php
@@ -0,0 +1,98 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>Пользовательское соглашение</title>
+<meta charset="utf-8">
+<meta name="description" content="Our Roadmap is a non-exhaustive list of the current goals we aim to achieve through new developments and continuous integration, made possible with the support of our generous patrons.">
+<meta name="keywords" content="rpcs3, playstation, playstation 3, ps3, emulator, debugger, windows, linux, macos, freebsd, open source, nekotekina, kd11, roadmap">
+<?php include 'lib/module/sys-meta.php';?>
+<meta property="og:title" content="RPCS3 - Roadmap" />
+<meta property="og:description" content="Our Roadmap is a non-exhaustive list of the current goals we aim to achieve through new developments and continuous integration, made possible with the support of our generous patrons." />
+<meta property="og:image" content="https://rpcs3.net/img/meta/mobile/1200.png" />
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="630" />
+<meta property="og:url" content="https://rpcs3.net" />
+<meta property="og:locale" content="en_US"/>
+<meta property="og:type" content="website" />
+<meta property="og:site_name" content="RPCS3" />
+
+<meta name="twitter:title" content="RPCS3 - Roadmap">
+<meta name="twitter:description" content="Our Roadmap is a non-exhaustive list of the current goals we aim to achieve through new developments and continuous integration, made possible with the support of our generous patrons.">
+<meta name="twitter:image" content="https://rpcs3.net/img/meta/mobile/1200.png">
+<meta name="twitter:site" content="@rpcs3">
+<meta name="twitter:creator" content="@rpcs3">
+<meta name="twitter:card" content="summary_large_image">
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+</head>
+<body>
+<?php include 'lib/module/sys-php.php';?>
+<div class="page-con-content">
+	<div class="banner-con-container darkmode-header">
+		<div id="object-particles">
+		</div>
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class="banner-con-backdrop darkmode-header">
+		</div>
+		<div class='banner-con-title fade-up-onstart'>
+			<div class='banner-tx1-title fade-up-onstart pulsate'>
+				<h1>Пользовательское соглашение</h1>
+			</div>
+			<div class='banner-con-divider'>
+			</div>
+			<div class='banner-tx2-title fade-up-onstart'>
+				<p>
+					 Перед оформлением заявки ознакомтесь с пользовательским соглашением
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class='container-con-block darkmode-block'>
+				<div class='container-con-wrapper'>
+					<div class='container-tx1-block darkmode-txt'>
+						<h2>1.Общие условия</h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p class="download-desc">
+						1.1. Любое использование наших продуктов или услуг означает, что вы полностью прочитали, поняли и согласны с условиями настоящего соглашения и обязуетесь их соблюдать.
+						<br>
+						1.2. Если вы не согласны с настоящими условиями, вы не должны устанавливать, использовать или иным образом получать доступ к нашему сервису и услугам.
+						<br>
+						1.3. Условия настоящего пользовательского соглашения являются существенными и обязательными для всех участников проекта.
+						<br>
+						1.4. Администрация Сайта оставляет за собой право в одностороннем порядке изменять настоящие Условия пользования сайтом (пользовательское соглашение), отдельных разделов Сайта в целом или частями без предварительного уведомления Пользователей Сайта.
+						<br>
+						1,5, Ничто в Соглашении не может пониматься как установление между Пользователем и Администрации Сайта агентских отношений, отношений товарищества, отношений по совместной деятельности, отношений личного найма, либо каких-то иных отношений, прямо не предусмотренных Соглашением.
+						<br>
+						</p>
+					</div>
+					<div class='container-tx1-block darkmode-txt'>
+						<h2>2. Обязательства Пользователя</h2>
+					</div>
+					<div class='container-tx2-block darkmode-txt'>
+						<p class="download-desc">
+						2.1. Пользователь соглашается не предпринимать действий, которые могут рассматриваться как нарушающие российское законодательство или нормы международного права.
+						<br>
+						2.2. Пользователь несет на себе полную ответственность за использование материалов, заказанных с Сайта.
+						<br>
+						2.3. При цитировании материалов Сайта, ссылка на Сайт обязательна.
+						<br>
+						2.4. Комментарии и иные записи Пользователя на Сайте не должны вступать в противоречие с требованиями общепринятых норм морали и нравственности.
+						<br>
+						2.5. Пользователь предупрежден о том, что Администрация Сайта не несет ответственности за посещение и использование им внешних ресурсов, ссылки на которые могут содержаться на сайте.
+						<br>
+						2.6. Пользователь согласен с тем, что Администрация Сайта не несет ответственности и не имеет прямых или косвенных обязательств перед Пользователем в связи с любыми прямыми и/или возможными убытками, включая упущенную выгоду, потерянные данные, вред чести, достоинству или деловой репутации и/или ущерб в любой форме, которые возникли или могут возникнуть из-за использования Сайта и его содержимого.
+						</p>
+					</div>
+				</div>
+</div>
+<?php include 'lib/module/inc-footer.php';?>
+</body>
+</html>
